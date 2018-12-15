8db6368374d2b9608ea38ddac9469c0cb0289da1
gennabarge <gennabarge@172.20.10.5>
2018-12-15 13:15:30 -0500

    wrong code this is the loose commit

--- src/Unit2ChoHanProject.java
+++ src/Unit2ChoHanProject.java
@@ -3,7 +3,22 @@ public class Unit2ChoHanProject {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int numberLoop = 1; 
+		int compWins = 0;
+		int playWins = 0;
+		for (int i=0; i < 10; i++) {
+			int whichDie = (int) (Math.random () * 2) +1;
+			if (whichDie == 1) {
+				RegularDice dice1 = new RegularDice();
+				RegularDice dice2 = new RegularDice();
+			}
+			else {
+				LoadedDice dice1 = new LoadedDice();
+				LoadedDice dice2 = new LoadedDice();
+			}
+			//start with randomizing the rolls 
+			int dieRoll = (int)(Math.random() * 6) + 1;
+			int playerRoll = (int)(Math.random() * 6) + 1;
 	}
 
 }
