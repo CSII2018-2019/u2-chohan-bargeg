e4a9793dd19d30ad84ab1f249952141ab6d0fda6
gennabarge <gennabarge@10.40.230.117>
2019-01-04 10:42:47 -0500

    Finished classes just having trouble
    -trouble with getRoll()
    - classes are finished

--- src/LoadedDice.java
+++ src/LoadedDice.java
@@ -1,11 +1,30 @@
 
 public class LoadedDice {
-	public LoadedDice() {
-		setnumSides (numSides);
+	private int numberSides;
+	private double roll;
+	public LoadedDice(int n) {
+		numberSides = n; 
+		setRoll();
 	}
 	
-	public void setSides (int n) {
+	public double setRoll () {
+		double oddOrEven = (Math.random() * 3 +1);
+		if (oddOrEven == 1) {
+			double roll = 1;
+			
+		}
+		if (oddOrEven == 2) {
+			double roll = 2;
+		}
+		else if (oddOrEven ==3) {
+			double roll = 1;
+		}
 		
+		return roll;
 	}
+	public double getRoll() {
+		return roll;
+	}
+		
 
 }
--- src/Unit2ChoHanProject.java
+++ src/Unit2ChoHanProject.java
@@ -1,11 +1,12 @@
+import javax.swing.JOptionPane;
 
 public class Unit2ChoHanProject {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int numberLoop = 1; 
-		int compWins = 0;
-		int playWins = 0;
+		int play1Wins = 0;
+		int play2Wins = 0;
 		for (int i=0; i < 10; i++) {
 			int whichDie = (int) (Math.random () * 2) +1;
 			if (whichDie == 1) {
@@ -14,13 +15,36 @@ public class Unit2ChoHanProject {
 				
 			}
 			else {
-				LoadedDice dice1 = new LoadedDice();
-				LoadedDice dice2 = new LoadedDice();
+				LoadedDice dice1 = new LoadedDice(10);
+				LoadedDice dice2 = new LoadedDice(10);
 			}
 			int totalRoll = dice1.getRoll() + dice2.getRoll(); 
 			//start with randomizing the rolls 
-			int dieRoll = (int)(Math.random() * 6) + 1;
-			int playerRoll = (int)(Math.random() * 6) + 1;
+			
+			String choHan;
+			if (totalRoll %2 ==0) {
+				choHan = "Han"; 
+			}
+			else {
+				choHan = "Cho";
+			}
+		String playerGuess = JOptionPane.showInputDialog("Would you like to guess Cho or Han?");
+		String opponentGuess = JOptionPane.showInputDialog("What is your guess opponent? Cho or Han?");
+		if (playerGuess.equals(choHan)) {
+			System.out.println("Congrats! You guessed right!");
+			play1Wins ++;
+		}
+		if (opponentGuess.equals(choHan)) {
+			System.out.println("Congrats! You guessed right!");
+			play2Wins++;
+		}
+		System.out.println("The scores after the rounnd... Player 1 score:" + play1Wins + ". Player 2 score:" + play2Wins);
+		
+		
+		
+			
+			
 	}
 
+}
 }
